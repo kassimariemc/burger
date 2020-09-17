0482cd40f0eed5111b92b5c05f77bc3dc284d6e3
Kassi Stumb <kassi.mcintosh@gmail.com>
2020-09-17 09:56:38 -0400

    server set up

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,4 +1,25 @@
 const express = require("express");
+const router = express.Router();
 const burger = require("../models/burger");
+
+// ROUTES
+// --------------------
+// Get route
+router.get("/", function(req, res) {
+
+});
+
+// Post route -> Create a new burger using the data posted from the front-end.
+router.post("/api/burgers", function(req, res) {
+  
+  res.json({ id: result.insertId });
+  console.log({ id: result.insertId });
+
+});
+
+// Update a burger devoured
+router.put("/api/burgers/:id", function(req, res) {
+
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,1 +1,24 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
+
+const PORT = process.env.PORT || 8081;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+const routes = require("./controllers/burgers_controller");
+
+app.use(routes);
+
+// Listener
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});

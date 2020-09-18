71e95777beea285570b591ceb6f11a2f421b88cc
Kassi Stumb <kassi.mcintosh@gmail.com>
2020-09-18 08:32:52 -0400

    orm complete

--- config/orm.js
+++ config/orm.js
@@ -1,3 +1,34 @@
-const connection = require("./connection");
+const connection = require("../config/connection.js");
 
-module.exports = new ORM(connection);
+const orm = {
+  selectAll: function() {
+    var queryString = "SELECT * FROM burgers";
+    connection.query(queryString, [table], 
+      function(err, result) {
+        if (err) throw err;
+        console.log(result);
+    });
+  },
+  insertOne: function(val) {
+    var queryString = "INSERT INTO burgers (burger_name) VALUES (?)";
+    console.log(queryString);
+    connection.query(queryString, [val], 
+      function(err, result) {
+        if (err) throw err;
+        console.log(result);
+    });
+  },
+  updateOne: function(val) {
+    var queryString = "UPDATE burgers SET devoured = false WHERE id = ?";
+
+    connection.query(
+      queryString,[val],
+      function(err, result) {
+        if (err) throw err;
+        console.log(result);
+      }
+    );
+  }
+};
+
+module.exports = new orm;

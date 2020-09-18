9c0da5b91c4541beab3e08350c847e7a21e9d811
Kassi Stumb <kassi.mcintosh@gmail.com>
2020-09-18 08:47:49 -0400

    burgers.js with ajax calls complete

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,37 @@
+$(function() {
+  $(".create-form").on("submit", function(event) {
+    event.preventDefault();
+
+    const newBurger = {
+      burger_name: $("#burger-input").val().trim(),
+    };
+
+    // Send the POST request.
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function() {
+        console.log("created new burger");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+  $(".change-devoured").on("click", function(event) {
+    const id = $(this).data("id");
+    const newDevoured = $(this).data("newdevoured");
+
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT"
+    }).then(
+      function() {
+        console.log("changed burger to devoured");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+})
